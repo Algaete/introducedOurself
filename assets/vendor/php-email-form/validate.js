01e50b21f172f21ab5afec0563032dfd7707dc48
Algaete <51843665+Algaete@users.noreply.github.com>
2022-07-17 16:19:20 -0400

    mail funcionando

--- assets/vendor/php-email-form/validate.js
+++ assets/vendor/php-email-form/validate.js
@@ -6,9 +6,62 @@
 (function () {
   "use strict";
 
-  let forms = document.querySelectorAll('.php-email-form');
+  const forms = document.querySelector('.php-email-form');
+    function sendMessage(e){
+      debugger;
+      e.preventDefault();
+      
+      const name = document.querySelector("[name='name']").value;
+      const email = document.querySelector("[name='email']").value;
+      var msg = document.querySelector("[name='message']").value;
+      msg = name + " " + email + " " + "<br>" + msg;
+      
 
-  forms.forEach( function(e) {
+      /*Email.send({
+          Host : "smtp.yourisp.com",
+          Username : "agaete92@gmail.com",
+          Password : "cottlgbcwzrbrdoj",
+          To : 'agaete92@gmail.com',
+          From : email,
+          Subject : "Contacto BigThinks",
+          Body : msg
+      }).then(
+        message => alert(message)
+      );*/
+
+      let thisForm = this;
+      thisForm.querySelector('.loading').classList.add('d-block');
+      thisForm.querySelector('.error-message').classList.remove('d-block');
+      thisForm.querySelector('.sent-message').classList.remove('d-block');
+
+      Email.send({
+          SecureToken : "d43c4130-aa0e-4334-bb2c-06ac685e2a00 ",
+          To : 'agaete92@gmail.com',
+          From : 'agaete92@gmail.com',
+          Subject : "Contacto BigThinks",
+
+          Body : msg
+      }).then(function (message) {
+
+        thisForm.querySelector('.loading').classList.remove('d-block');
+
+        if(message==="OK"){
+          thisForm.querySelector('.sent-message').classList.add('d-block');
+          thisForm.reset();
+        }else{
+          displayError(thisForm, "error");
+        }
+       
+      });
+        
+    }
+
+    forms.addEventListener('submit',sendMessage);
+
+
+
+
+ /* forms.forEach( function(e) {
     e.addEventListener('submit', function(event) {
       event.preventDefault();
 
@@ -74,12 +127,13 @@
     .catch((error) => {
       displayError(thisForm, error);
     });
-  }
+  }*/
 
   function displayError(thisForm, error) {
     thisForm.querySelector('.loading').classList.remove('d-block');
     thisForm.querySelector('.error-message').innerHTML = error;
     thisForm.querySelector('.error-message').classList.add('d-block');
   }
+ 
 
 })();
